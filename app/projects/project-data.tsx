02cfee5060a15b86c352c7796238d73f39fb56f6
CowCurry <george.weale@gmail.com>
2024-10-23 16:27:51 -0400

    added projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,31 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
+    title: "Pico Size Chatbot",
+    year: 2024,
+    description:
+      "Built a chatbot from scratch only using Pytorch and numpy. Trained on school cluster.",
+    url: "https://example.com/",
+  },
+  {
+    title: "MealMapper - Columbia Startup Lab",
+    year: 2024,
+    description:
+      "Delivered a full stack development of an AI meal tracking app for use by highschool sports teams and local nutritionist centers to quantitatively assess food cataloging, adopted by Los Altos High Football Team. Pytorch, YOLOv8, AWS",
+    url: "https://example.com/",
+  },
+  {
+    title: "EarliBird - UCSB New Venture Incubator",
     year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      "Led development of a neural network-driven process for two system chemical plants using tensorflow, utilizing stochastic modeling and aspens equations of state to optimize plant cost, developed DLMPC package on Matlab",
     url: "https://example.com/",
   },
   {
-    title: "Project Two",
+    title: "SeePrint - UCSB New Venture Incubator",
     year: 2022,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      "Developed a natural language processes system using tensorflow uses the existing cash registers to optimize store item placement and supply chain restocking, while reducing upfront costs by 200x than cloud business",
     url: "https://example.com/",
   },
 ];
