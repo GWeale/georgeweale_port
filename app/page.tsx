880a1b2bbf3ad8a2570c429c8ca4f9844df096fb
CowCurry <george.weale@gmail.com>
2024-10-23 14:59:12 -0400

    added bio and updated config

--- app/page.tsx
+++ app/page.tsx
@@ -36,31 +36,6 @@ export default function Page() {
           </a>
           .
         </p>
-        <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
-        </p>
-        <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
-        </p>
       </div>
     </section>
   );
